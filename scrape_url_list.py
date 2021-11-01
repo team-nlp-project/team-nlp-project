1dd8b671082503ea4dce1d7c212471aa7e362718
adam-talbot <adam.talbot527@gmail.com>
2021-11-01 13:37:59 -0500

    added function to get most forked list

--- scrape_url_list.py
+++ scrape_url_list.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from time import strftime
 import numpy as np
+from time import sleep
 
 def get_new_links():
     '''
@@ -22,3 +23,25 @@ def get_cached_links():
     '''
     cached_links = np.load(f'github_links-2021-10-29.npy').tolist() # change this name to match file name in repo
     return cached_links
+
+def get_most_forked():
+    cards_list = []
+    link_list = []
+    for i in range(1,51):
+        url = 'https://github.com/search?o=desc&p={}&q=stars%3A%3E1&s=forks&type=Repositories'
+        url = url.format(i)
+        response = requests.get(url, headers={"user-agent": "Codeup"})
+        print(response.status_code)
+        soup = BeautifulSoup(response.text)
+        cards = soup.select('.repo-list-item')
+        cards_list.append(cards)
+        #print(i)
+        #print(url)
+        #print(len(cards))
+        #print(len(cards_list))
+        for card in cards:
+            link = card.select_one('.v-align-middle').attrs['href']
+            link_list.append(link)
+        sleep(30)
+    [link[1:] for link in link_list]
+    return link_list
