ab503aa860a5a620013f9d26b21ecfc32841af1b
adam-talbot <adam.talbot527@gmail.com>
2021-11-01 12:22:07 -0500

    updated prep module so we can all explore on same data

--- prepare.py
+++ prepare.py
@@ -9,6 +9,8 @@ import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
 
+from sklearn.model_selection import train_test_split
+
 
 def basic_clean(some_string):
     '''
@@ -64,4 +66,22 @@ def prep_nlp(df, original_text_col = 'original', extra_words = [], exclude_words
     df['clean'] = df[original_text_col].apply(basic_clean).apply(tokenize).apply(remove_stopwords, extra_words, exclude_words)
     df['stemmed'] = df.clean.apply(stem)
     df['lemmatized'] = df.clean.apply(lemmatize)
+    df = df.dropna()
+    top4 = ['JavaScript', 'Java', 'Python', 'C++']
+    df.language = df.language.apply(lambda x: 'Other' if x not in top4 else x)
+    df = df[df.language != 'Other']
     return df
+
+def split_readme(df):
+    '''
+    This function takes in a df and splits it into train, validate, and test dfs
+    final proportions will be 60/20/20 for train/validate/test
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.language)
+    train, validate = train_test_split(train_validate, test_size=.25, random_state=123, stratify=train_validate.language)
+    train_prop = train.shape[0] / df.shape[0]
+    val_prop = validate.shape[0] / df.shape[0]
+    test_prop = test.shape[0]/df.shape[0]
+    print(f'Train Proportion: {train_prop:.2f} ({train.shape[0]} rows)\nValidate Proportion: {val_prop:.2f} ({validate.shape[0]} rows)\
+    \nTest Proportion: {test_prop:.2f} ({test.shape[0]} rows)')
+    return train, validate, test
